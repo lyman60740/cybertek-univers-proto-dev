14ec0c9133f072372646026882b63bc37e077b39
Lyman ABID <lym_60740@hotmail.fr>
2025-07-16 16:22:55 +0200

    rollback avant reduction scroll mobile

--- src/CameraRigCarShow.js
+++ src/CameraRigCarShow.js
@@ -87,7 +87,7 @@ export const CameraRigCarShow = ({ carRef, carReady, carPosition }) => {
         scrollTrigger: {
           trigger: '.carshow-container',
           start: isMobile ? 'top top' : 'top top',
-          end: '+=4000px',
+          end: isMobile ? '+=4000px' : '+=2000px',
           scrub: isMobile ? 1 : 2,
           pin: true,
           pinSpacing: true,
